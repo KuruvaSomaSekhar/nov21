c8a906b74b3252107cf77ac1e03b670d4c39b5c7
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-15 07:46:15 +0530

    updated command

--- Jenkinsfile_build.groovy
+++ Jenkinsfile_build.groovy
@@ -5,6 +5,7 @@ pipeline {
         stage("Checkout code") {
             steps {
                 println "Clone our code to our repo"
+                sh "ls -l"
             }
         }
         stage("Build code"){
@@ -12,7 +13,7 @@ pipeline {
                 println "mvn clean package"
             }
         }
-        stage("Upload artifacts to s3"){
+        stage("Upload artifacts to S3"){
             steps {
                 println "Uploading artifacts to s3 bucket"
             }
