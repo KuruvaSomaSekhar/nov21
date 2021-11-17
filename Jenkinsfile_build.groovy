252d59e49703fff2bb93178d23b7b75a12a11494
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-17 08:09:39 +0530

    updated syntax

--- Jenkinsfile_build.groovy
+++ Jenkinsfile_build.groovy
@@ -9,7 +9,7 @@ pipeline {
             steps {
                 println "Clone our code to our repo"
                 println $BRANCH
-                println ${BRANCH}
+                println "${BRANCH}"
                 sh "ls -l"
                 checkout([$class: 'GitSCM',branches: [[name: '*/$BRANCH']], userRemoteConfigs: [[ url: 'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git']]])
                 sh "ls -lart ./*"
