e89e0657f8a574afd81f0a5d2d7a3afe17a7b445
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-17 07:52:33 +0530

    Added variable section

--- Jenkinsfile_build.groovy
+++ Jenkinsfile_build.groovy
@@ -1,12 +1,15 @@
 //Declarative pipeline
 pipeline {
     agent any
+    environment {
+        BRANCH = "${env.BRANCH_NAME}"
+    }
     stages {
         stage("Checkout code") {
             steps {
                 println "Clone our code to our repo"
                 sh "ls -l"
-                checkout([$class: 'GitSCM',branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git']]])
+                checkout([$class: 'GitSCM',branches: [[name: '*/${BRANCH}']], userRemoteConfigs: [[ url: 'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git']]])
                 sh "ls -lart ./*"
             }
         }
@@ -21,7 +24,7 @@ pipeline {
             steps {
                 println "Uploading artifacts to s3 bucket"
                 sh "echo $BUILD_NUMBER"
-                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://somuart/master/${BUILD_NUMBER}/"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://somuart/${BRANCH}/${BUILD_NUMBER}/"
             }
         }
     }
