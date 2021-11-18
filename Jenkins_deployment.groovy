ec62b41ff3f8b6cba4afd61e89230bb7b9929ff3
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-18 07:30:53 +0530

    updated script

--- Jenkins_deployment.groovy
+++ Jenkins_deployment.groovy
@@ -1,10 +1,20 @@
 //declarative pipeline
 pipeline{
     agent any
+   parameters {
+    string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'From which branch artifacts want to deploy?')
+    string(name: 'BUILD_NUM', defaultValue: '', description: 'From which build number artifacts want to deploy?')
+  }
     stages{
         stage("download artifact"){
             steps {
                 println "Here I'm downloading artifacts from S3"
+                sh """
+                        aws s3 ls
+                        aws s3 ls s3://somuart
+                        aws s3 ls s3://somuart/${BRANCH_NAME}/${BUILD_NUM}/
+
+                   """
                 
             }
         }
