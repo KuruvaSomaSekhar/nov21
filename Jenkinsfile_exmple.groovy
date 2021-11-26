5f81ac0ba74972ca1b28b64cc17d8bf90abfd5ef
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-26 08:36:43 +0530

    updated logic

--- Jenkinsfile_exmple.groovy
+++ Jenkinsfile_exmple.groovy
@@ -1,10 +1,8 @@
 pipeline {
     agent any
-    environment {
-        ENV_NAME = "${env.BRANCH_NAME}"
-    }
-    parameters{
-        string(name: ENVIRONMENT_NAME , defaultValue:QA,description:"")
+     parameters{
+        //string(name: ENVIRONMENT_NAME , defaultValue:QA,description:"")
+        string(name: 'ENVIRONMENT_NAME', defaultValue: '', description: '')
     }
 
     // ----------------
@@ -15,18 +13,19 @@ pipeline {
                 echo 'Building Container..'
 
                 script {
-                    if (ENVIRONMENT_NAME == 'QA') {
+                    if (${ENVIRONMENT_NAME} == 'QA') {
                         SERVERS = ["1.1.1.1",""]
-                    } else if (ENVIRONMENT_NAME == 'DEV') {
+                    } else if (${ENVIRONMENT_NAME} == 'DEV') {
                         SERVERS = ["1.1.2.1","1.1.2.2"]
                     }
                     println SERVERS.size()
+                   // echo SERVERS
                 }
-                echo 'Building Branch: ' + env.BRANCH_NAME
-                echo 'Build Number: ' + env.BUILD_NUMBER
-                echo 'Building Environment: ' + ENV_NAME
+                // echo 'Building Branch: ' + env.BRANCH_NAME
+                // echo 'Build Number: ' + env.BUILD_NUMBER
+                // echo 'Building Environment: ' + ENV_NAME
 
-                echo "Running your service with environemnt ${ENV_NAME} now"
+                // echo "Running your service with environemnt ${ENV_NAME} now"
             }
         }
     }
