6b5bd172ceebd98ec3305c404e0d23c100b1323f
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-26 07:31:13 +0530

    added multiple server groovy file

---
+++ Jenkinsfile_multiple.groovy
@@ -0,0 +1,22 @@
+//declarative pipeline
+pipeline{
+    agent any
+    parameters { string(name: 'SERVERIPS', defaultValue: '', description: '') }
+    stages{
+        stage("Multiple servers")
+        {
+            steps {
+                sh """
+                   IFS=',' read -ra ADDR <<< "${SERVERIPS}"
+                    for ip in "${ADDR[@]}"; 
+                    do
+                    echo $ip
+                     # process "$i"
+                    done
+                   
+                   
+                   """
+            }
+        }
+    }
+}
