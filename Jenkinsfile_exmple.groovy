45244589fe79707b0048034ca22b9f8bca5b897d
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-26 08:43:52 +0530

    updated logic3

--- Jenkinsfile_exmple.groovy
+++ Jenkinsfile_exmple.groovy
@@ -19,6 +19,13 @@ pipeline {
                         SERVERS = ["1.1.2.1","1.1.2.2","1.1.2.3"]
                     }
                     println SERVERS.size()
+                    for(item in SERVERS){
+                     println item
+                     sh '''
+                        echo "download warfirle from s3"
+                        echo "scp file to ${item}"
+                        '''
+                        }
                    // echo SERVERS
                 }
                 // echo 'Building Branch: ' + env.BRANCH_NAME
