47384225ac82d2f5f1db31a58a22ec9e422039a1
kuruvasomasekhar <KuruvaSomaSekhar@users.noreply.github.com>
2021-11-26 08:20:25 +0530

    updated servers

--- Jenkinsfile_groovyarray.groovy
+++ Jenkinsfile_groovyarray.groovy
@@ -4,9 +4,9 @@ pipeline {
     if(${ENV} == QA) {   
         SERVERS = ["1.1.1.1","1.1.1.2"]
     } else if(${ENV} == STAGE) {   
-	    SERVERS = [1.1.2.1,1.1.2.2,1.1.2.3]  
+	    SERVERS = ["1.1.2.1","1.1.2.2","1.1.2.3"]  
     } else if(${ENV} == STAGE) { 	
-	    SERVERS = [1.1.3.1,1.1.3.2]  
+	    SERVERS = ["1.1.3.1","1.1.3.2"]  
     } else {  
 	    println "You didn't provide any environment"
     }
